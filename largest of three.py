bd717c024d50d0517a78604f569fbf4440e4fbf1
Darsh Jain <65119169+darshjain@users.noreply.github.com>
2020-05-15 23:06:00 +0530

    Add files via upload

---
+++ largest of three.py
@@ -0,0 +1,11 @@
+
+a=input("Enter a,b and c \n")
+b=input()
+c=input()
+
+if a>b and a>c:
+    print(a," is the largest")
+elif b>c and b>a:
+    print(b," is the largest")
+else :
+    print(c," is the largest")
